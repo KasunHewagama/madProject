8b6f3c99ebb926620b1a5837518dc08c4722cd08
KasunHewagama <kghewagama1997@gmail.com>
2019-08-09 00:14:50 +0530

    Walking home & Running home pages linked successfully...!!

--- app/src/main/java/com/example/madproject/runningHomepage.java
+++ app/src/main/java/com/example/madproject/runningHomepage.java
@@ -1,14 +1,27 @@
 package com.example.madproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.os.Bundle;
+import android.view.View;
 
-public class runningHomepage extends AppCompatActivity {
+public class runningHomepage extends AppCompatActivity implements View.OnClickListener {
 
+//    private CardView add,view,done;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_running_homepage);
+
+//        define variables
+//        add = (CardView) findViewById(R.id.cardId5);
+//        view = (CardView) findViewById(R.id.cardId6);
+//        done = (CardView) findViewById(R.id.cardId7);
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
