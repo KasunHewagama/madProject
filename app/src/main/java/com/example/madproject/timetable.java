d4e6493fa7e8e506d42dbb9bddb993e8d7ff1b7f
PMGurusinghe <madhushangurusinghe@gmail.com>
2019-08-08 11:14:20 +0530

    new timetable

--- app/src/main/java/com/example/madproject/timetable.java
+++ app/src/main/java/com/example/madproject/timetable.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Timetable extends AppCompatActivity {
+public class timetable extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
