e0c04c00911f6dc19d32c77aa10a6dd6b4da4b67
PMGurusinghe <madhushangurusinghe@gmail.com>
2019-08-08 17:18:29 +0530

    time table inside activities created

--- app/src/main/java/com/example/madproject/newaddexam.java
+++ app/src/main/java/com/example/madproject/newaddexam.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Results extends AppCompatActivity {
+public class newaddexam extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_results);
+        setContentView(R.layout.activity_newaddexam);
     }
 }
--- app/src/main/java/com/example/madproject/newaddresults.java
+++ app/src/main/java/com/example/madproject/newaddresults.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class addcourse extends AppCompatActivity {
+public class newaddresults extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_addcourse);
+        setContentView(R.layout.activity_newaddresults);
     }
 }
