d0c5bf8677457fa02daa95df724cd3c4eeebcbfc
YuvinR <yuvinransika1998@gmail.com>
2019-08-08 08:58:34 +0530

    new home screen added

--- app/src/main/java/com/example/madproject/MainActivity.java
+++ app/src/main/java/com/example/madproject/MainActivity.java
@@ -1,56 +1,53 @@
 package com.example.madproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.gridlayout.widget.GridLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
-import static com.example.madproject.R.id.grid1;
-
-public class MainActivity extends AppCompatActivity  {
+import android.widget.Switch;
 
 
-    private Button work,holiday,timetable,community;
+public class MainActivity extends AppCompatActivity implements  View.OnClickListener {
+
+
+    private CardView work,holiday,timetable,community;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-      //defining layouts
-       work =(Button) findViewById(R.id.button4);
-       holiday=(Button) findViewById(R.id.button5);
-       timetable =(Button) findViewById(R.id.button6);
-       community=(Button) findViewById(R.id.button7);
+        work=(CardView)findViewById(R.id.cardId1);
+        holiday=(CardView)findViewById(R.id.cardId2);
+        timetable=(CardView)findViewById(R.id.cardId3);
+        community=(CardView)findViewById(R.id.cardId4);
 
-    work.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            startActivity(new Intent(MainActivity.this,workoutHomepage.class));
+        work.setOnClickListener(this);
+        holiday.setOnClickListener(this);
+        timetable.setOnClickListener(this);
+        community.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent i;
+
+        switch(view.getId())
+        {
+            case R.id.cardId1 : i=new Intent(this,workoutHomepage.class);startActivity(i);break;
+
+            case R.id.cardId2 : i=new Intent(this,Holiday_UI1.class);startActivity(i);break;
+
+            case R.id.cardId3 : i=new Intent(this,Timetable.class);startActivity(i);break;
+
+            case R.id.cardId4 : i=new Intent(this,workoutHomepage.class);startActivity(i);break;
+
+            default:break;
         }
-    });
 
-        holiday.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this,Holiday_UI1.class));
-            }
-        });
-
-        timetable.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this,Timetable.class));
-            }
-        });
-
-
-
-
-//    }
-
-
-}}
+    }
+}
