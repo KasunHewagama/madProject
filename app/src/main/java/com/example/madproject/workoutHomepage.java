58d4bc4ceb9c7f0fa82f8a55fceca21593490271
KasunHewagama <kghewagama1997@gmail.com>
2019-08-08 12:35:23 +0530

    minor changes to walkingHome page

--- app/src/main/java/com/example/madproject/workoutHomepage.java
+++ app/src/main/java/com/example/madproject/workoutHomepage.java
@@ -9,23 +9,23 @@ import android.widget.Button;
 
 public class workoutHomepage extends AppCompatActivity {
 
-    private Button run,walk,cycle,swim;
+   // private Button run,walk,cycle,swim;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_workouthomepage);
 
         //define layouts
-        run = (Button) findViewById(R.id.button11);
-        walk = (Button) findViewById(R.id.button12);
-        cycle = (Button) findViewById(R.id.button13);
-        swim = (Button) findViewById(R.id.button14);
+//        run = (Button) findViewById(R.id.button11);
+//        walk = (Button) findViewById(R.id.button12);
+//        cycle = (Button) findViewById(R.id.button13);
+//        swim = (Button) findViewById(R.id.button14);
 
-        run.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(workoutHomepage.this,walkingHomepage.class));
-            }
-        });
+//        run.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                startActivity(new Intent(workoutHomepage.this,walkingHomepage.class));
+//            }
+//        });
     }
 }
