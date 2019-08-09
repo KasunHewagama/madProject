5e9e80523244d5eebce59d15974b81f9913e8880
YuvinR <yuvinransika1998@gmail.com>
2019-08-09 09:29:22 +0530

    Last Final

--- app/src/main/java/com/example/madproject/addPlan.java
+++ app/src/main/java/com/example/madproject/addPlan.java
@@ -2,13 +2,36 @@ package com.example.madproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class addPlan extends AppCompatActivity {
+public class addPlan extends AppCompatActivity implements View.OnClickListener {
 
+    private Button add;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_plan);
+
+//        define variables
+        add = (Button) findViewById(R.id.button2);
+
+//        define onClickListner method for each page
+        add.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent06;
+
+        switch (view.getId()){
+            case R.id.cardId5: intent06 = new Intent(this,addView.class);
+                startActivity(intent06);
+                break;
+            default:break;
+        }
     }
 }
--- app/src/main/java/com/example/madproject/addView.java
+++ app/src/main/java/com/example/madproject/addView.java
@@ -2,13 +2,37 @@ package com.example.madproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class addView extends AppCompatActivity {
+public class addView extends AppCompatActivity implements View.OnClickListener {
 
+    private Button done;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_view);
+
+//        define variables
+        done = (Button) findViewById(R.id.button7);
+
+//        define OnClickListner method to each page
+
+        done.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent07;
+
+        switch (view.getId()){
+            case R.id.cardId5: intent07 = new Intent(this,donnedWorkout.class);
+                startActivity(intent07);
+                break;
+            default:break;
+        }
+    }
+
 }
