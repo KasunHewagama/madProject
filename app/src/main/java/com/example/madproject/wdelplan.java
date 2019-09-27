0c94c0e0ad262e38c2f6e00f34587a6f53e1fcab
TaneeshaDew <it18164404@my.sliit.lk>
2019-09-27 11:34:26 +0530

    Holiday is done

--- app/src/main/java/com/example/madproject/wdelplan.java
+++ app/src/main/java/com/example/madproject/wdelplan.java
@@ -11,6 +11,7 @@ public class wdelplan  extends Application {
     public String gvalue_workoutName;
     public String gvalue_startingTime;
 
+
     public String getGvalue_workoutName() {
         return gvalue_workoutName;
     }
@@ -25,5 +26,37 @@ public class wdelplan  extends Application {
 
     public void setGvalue_startingTime(String gvalue_startingTime) {
         this.gvalue_startingTime = gvalue_startingTime;
+    }
+
+
+
+    ///////////////////////////
+    public ArrayList<String> Harrlist=new ArrayList<>();
+    public ArrayAdapter<String> Harradp;
+    public String gvalue_hname;
+    public String getGvalue_hdate;
+
+    public String getValue_name() {
+        return gvalue_hname;
+    }
+
+    public void setValue_name(String value_name) {
+        this.gvalue_hname = value_name;
+    }
+
+    public String getGetGvalue_hdate() {
+        return getGvalue_hdate;
+    }
+
+    public void setGetGvalue_hdate(String getGvalue_hdate) {
+        this.getGvalue_hdate = getGvalue_hdate;
+    }
+
+    public ArrayList<String> getHarrlist() {
+        return Harrlist;
+    }
+
+    public void setHarrlist(ArrayList<String> harrlist) {
+        Harrlist = harrlist;
     }
 }
